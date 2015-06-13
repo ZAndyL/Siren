17739db8123092c6e232ba95e922d1dc6eb37b58
Uzair Haq <uzairhaq@Uzairs-MacBook-Pro.local>
2015-06-13 13:39:13 -0400

    Logic inside the button

--- app/src/main/java/com/zandyl/siren/MainActivity.java
+++ app/src/main/java/com/zandyl/siren/MainActivity.java
@@ -3,11 +3,14 @@ package com.zandyl.siren;
 import android.app.Activity;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.koushikdutta.async.future.FutureCallback;
@@ -25,7 +28,41 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(),"download started", Toast.LENGTH_SHORT).show();
+        Button talkButton = (Button)findViewById(R.id.Talk);
+        talkButton.setOnClickListener(talkButtonListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private View.OnClickListener talkButtonListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            talkButton();
+        }
+    };
+
+    public void talkButton() {
+        Toast.makeText(getApplicationContext(), "download started", Toast.LENGTH_SHORT).show();
+
         Ion.with(getApplicationContext())
                 .load("http://tts-api.com/tts.mp3?q=hello+world.")
                 .write(new File("/sdcard/test.mp3"))
@@ -52,27 +89,5 @@ public class MainActivity extends Activity {
                         }
                     }
                 });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
